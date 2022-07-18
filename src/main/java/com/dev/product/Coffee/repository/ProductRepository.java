489b5f611daf8752627479f4454bb59077f35292
Mai Ngọc Định <85937956+ngocdinh2k1@users.noreply.github.com>
2022-07-18 22:19:40 +0700

    Create query

--- src/main/java/com/dev/product/Coffee/repository/ProductRepository.java
+++ src/main/java/com/dev/product/Coffee/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.dev.product.Coffee.repository;
 
 import com.dev.product.Coffee.entity.ProductEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,10 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     
-    // Select * FROM product where price >= min and price <= max;
+    /* Select * FROM product where price >= min and price <= max; */
     @Query("from products p where p.price>=:min and p.price <=:max ")
     List<ProductEntity> selectByProductByPriceRange(BigDecimal min, BigDecimal max);
+    
+    @Query("from products p where p.title = ?1")
+    List<ProductEntity> selectProductByTitle(String title, Sort sort);
 }
--- src/test/java/com/dev/product/Coffee/CoffeeApplicationTests.java
+++ src/test/java/com/dev/product/Coffee/CoffeeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.dev.product.Coffee;
 
+import com.dev.product.Coffee.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class CoffeeApplicationTests {
 
+	@Autowired
+	private ProductRepository repository;
+	
 	@Test
 	void contextLoads() {
 	}
+
 
 }
